761d7b7772f5811ae55dbdf7bbe50c30663000fd
0x20h <kohj@informatik.uni-marburg.de>
2013-09-03 09:01:14 +0200

    added basic player controls+logic

--- src/jquery.trex.js
+++ src/jquery.trex.js
@@ -7,6 +7,10 @@
 				 * smaller values speed playback up. (speed > 0)
 				 */
 				speed: 1,	
+				/**
+				 * Automatically start playing when initialized.
+				 */
+				auto_start: false
 			};
 	
 	function Trex(element, options) {
@@ -18,6 +22,10 @@
 	}
 
 	Trex.prototype = {
+		/**
+		 * Initialize the element, the Terminal emulator and the
+		 * player.
+		 */
 		init: function() {
 			var self = this;
 			// read which session to load
@@ -31,6 +39,9 @@
 
 			$.getJSON(this.session, {},
 				function(data) {
+					self.script = data.script;
+					self.timing = data.timing;
+
 					self.term = new Terminal({
 						cols: parseInt(data.cols),
 						rows: parseInt(data.rows),
@@ -39,36 +50,131 @@
 					});
 
 					self.term.open(self.element);
-					self.script = data.script;
-					self.timing = data.timing;
-					self.player = {
-						current: 0,
-						// ignore first line of the script file
-						offset: data.script.indexOf("\n")
-					};
-					self.tick();
+					self.reset();
+					self.initControls();
+					if (self.settings.auto_start) {
+						self.tick();
+					}
 				}
 			);
 		},
 
-		tick: function() {
-			var self = this;
-
-			if (!this.timing[this.player.current]) {
+		/**
+		 * Move the player ticks forward and play.
+		 *
+		 * @param {Number} ticks Number of ticks to move forward [default=1]
+		 */
+		tick: function(ticks) {
+			var self = this,
+					ticks = ticks || 1;
+			
+			if (this.timing.length <= this.player.current) {
 				return;
 			}
 
-			var element = this.timing[this.player.current++];
-			
-			setTimeout(function() {
-				var count = parseInt(element[1])
-				var content = self.script.substr(self.player.offset, count)
+			var update = function() {
+				var count = 0;
 
+				for (var i=0; i < ticks; i++) {
+					var element = self.timing[self.player.current++];
+					count += parseInt(element[1])
+				}
+
+				var content = self.script.substr(self.player.offset, count)
 				self.player.offset += count;
 				self.term.write(content);
-				self.tick();
-			}, element[0] * 1000 * this.settings.speed)
+
+				if (self.player.playing) {
+					self.tick();
+				}
+			};
+
+			// fast-forward	
+			if (ticks > 1) {
+				update();
+			} else {
+				this.timer = setTimeout(
+					update, 
+					this.timing[this.player.current][0] * 1000 * this.settings.speed
+				);
+			}
 		},
+		
+
+		/**
+		 * Reset player for the first position.
+		 */
+		reset: function() {
+			this.player = {
+				current: 0,
+				// ignore first line of the script file
+				offset: this.script.indexOf("\n"),
+				playing: false
+			};
+
+			// clear pending timeouts
+			clearTimeout(this.timer);
+			this.term.reset();
+			this.term.focus();
+		},
+
+		/**
+		 * move the player to the given position.
+		 *
+		 * @param {Number} index The target index.
+		 */
+		jump: function(index) {
+			this.reset();
+			this.tick(index);
+		},
+
+		/**
+		 * Toggle playmode (play/pause).
+		 */
+		toggle: function() {
+			this.player.playing = !this.player.playing;
+
+			if (this.player.playing) {
+				this.tick();
+			}
+
+			this.term.focus();
+		},
+
+		/**
+		 * initialize player controls
+		 */
+		initControls: function() {
+			var self = this;
+			// initialize player controls
+			var pause = $('<input class="pause" value="pause" type="button"/>')
+				.bind('click', function() {
+					self.toggle();
+			});
+
+			var reset = $('<input value="reset" type="button"/>')
+				.bind('click', function() {
+					self.jump(0);
+			});
+
+			var faster = $('<input value="+" type="button"/>')
+				.bind('click', function() {
+					self.settings.speed = self.settings.speed > .25 ? self.settings.speed / 2 : .25;
+					self.term.focus();
+			});
+
+			var slower = $('<input value="-" type="button"/>')
+				.bind('click', function() {
+					self.settings.speed = self.settings.speed < 2 ? self.settings.speed * 2 : 2; 
+					self.term.focus();
+			});
+
+			$(this.element)
+				.before(pause)
+				.before(reset)
+				.before(faster)
+				.before(slower);
+		}
 	};
 
 	$.fn[pluginName] = function (options) {
